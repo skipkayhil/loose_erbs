cddb16125baea0e7d34a081936443739a6e4b394
Hartley McGuire <skipkayhil@gmail.com>
2024-01-31 12:47:46 -0500

    Parse and print dependencies in app/views
    This sets up the basic first step which is getting all of the view
    partials in an app and parsing their dependencies using the Action View
    Dependency Tracker.
    
    The CLI currently prints all views and their dependencies:
    
    ```
    $ bundle exec loose_erbs
    /home/hartley/test/dm/app/views/layouts/application.html.erb
    /home/hartley/test/dm/app/views/posts/_form.html.erb
    /home/hartley/test/dm/app/views/posts/_post.html.erb
    /home/hartley/test/dm/app/views/posts/edit.html.erb
    └── /home/hartley/test/dm/app/views/posts/form
    /home/hartley/test/dm/app/views/posts/index.html.erb
    └── posts/post
    /home/hartley/test/dm/app/views/posts/new.html.erb
    └── /home/hartley/test/dm/app/views/posts/form
    /home/hartley/test/dm/app/views/posts/show.html.erb
    └── posts/post
    ```

--- lib/loose_erbs.rb
+++ lib/loose_erbs.rb
@@ -1,8 +1,83 @@
 # frozen_string_literal: true
 
+require "action_view"
+require "action_view/dependency_tracker"
+
 require_relative "loose_erbs/version"
 
 module LooseErbs
-  class Error < StandardError; end
-  # Your code goes here...
+  singleton_class.attr_accessor :parser_class
+
+  self.parser_class = if defined?(ActionView::DependencyTracker::RubyTracker)
+    ActionView::DependencyTracker::RubyTracker
+  else
+    ActionView::DependencyTracker::RipperTracker
+  end
+
+  class Template
+    class << self
+      def from(path)
+        new(path, File.read(path))
+      end
+    end
+
+    attr_reader :path, :source
+
+    def initialize(path, source)
+      @path = path
+      @source = source
+    end
+
+    def dependencies
+      LooseErbs.parser_class.call(path, self)
+    end
+
+    def handler
+      ActionView::Template::Handlers::ERB
+    end
+
+    def type
+      ["text/html"]
+    end
+
+    def inspect
+      path
+    end
+  end
+
+  class Registry
+    def initialize
+      @map = {}
+    end
+
+    def print
+      @map.values.each do |template|
+        puts template.inspect
+
+        template.dependencies.each do |dependency|
+          puts "└── #{dependency}"
+        end
+      end
+    end
+
+    def register(path)
+      @map[path] = Template.from(path)
+    end
+  end
+
+  class Cli
+    def run
+      registry = Registry.new
+
+      views.each { registry.register(_1) }
+
+      registry.print
+    end
+
+    private
+
+    def views
+      Dir["#{Dir.pwd}/app/views/**/*.html.erb"]
+    end
+  end
 end
